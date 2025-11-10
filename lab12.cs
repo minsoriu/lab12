7dddc6da2feb6005a9c09f94899d53312e1b6abc
minsoriu <kwizzsisters@gmail.com>
2025-11-10 14:39:17 +0300

    Update lab12.cs

--- lab12.cs
+++ lab12.cs
@@ -1,3 +1,12 @@
 ﻿string s1 = Console.ReadLine();
 string s2 = Console.ReadLine();
 Console.WriteLine(s1.Replace(s2, ""));
+try
+{
+    int index = s1.IndexOf(s2);
+    Console.WriteLine(s1.Remove(index, s2.Length));
+}
+catch
+{
+    Console.WriteLine(s1);
+}
